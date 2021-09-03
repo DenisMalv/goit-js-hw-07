361069e037d144aeb8e00f1139fca514f0b44472
DenisMalv <kissed03@gmail.com>
2021-09-03 22:40:43 +0300

    непонятки с 7й дз

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,9 @@
 const input = document.querySelector('#font-size-control');
 const text = document.querySelector('#text');
-console.log(input)
+console.dir(input)
 console.log(text)
 console.log(`Стартовое значение ${input.value = 20} px`)
 input.addEventListener(
-    'input',
-    getSizeText => (
-        text.style.fontSize = `${getSizeText.currentTarget.value}px`,
-        console.log(getSizeText.currentTarget.value)),
-);
+    'input', changeValue => (text.style.fontSize = `${changeValue.currentTarget.value}.px`, console.log(changeValue.currentTarget.value)),
+)
+
